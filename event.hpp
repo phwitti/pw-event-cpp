56760063df1f20057d36b61ec48a6e30d6852621
Philipp M. Wittershagen <philippwittershagen@web.de>
2018-06-02 01:58:12 +0200

    - removed const_delegate - removed const_event - added std::function support in events (not removable)

--- event.hpp
+++ event.hpp
@@ -1,6 +1,7 @@
 #ifndef __PW_EVENT_HPP__
 #define __PW_EVENT_HPP__
 
+#include <functional>
 #include <list>
 #include "delegate.hpp"
 
@@ -10,14 +11,17 @@ namespace pw
     template <typename>
     class event;
 
-    template <typename>
-    class const_event;
-
     template <typename _ReturnType, typename... _Params>
     class event<_ReturnType(_Params...)>
     {
 
     public:
+
+        template<typename... _FunctionParams>
+        void add(_FunctionParams&&... _params)
+        {
+            m_functions.emplace_back(std::forward<_FunctionParams>(_params)...);
+        }
 
         void add(delegate<_ReturnType(_Params...)> &_delegate)
         {
@@ -56,25 +60,19 @@ namespace pw
         template<_ReturnType(*_Function)(_Params...)>
         void remove()
         {
-            delegate<_ReturnType(_Params...)> remove_delegate;
-            remove_delegate.bind<_Function>();
-            m_delegates.remove(remove_delegate);
+            m_delegates.remove(delegate<_ReturnType(_Params...)>::create<_Function>());
         }
 
         template <class _ObjectType, _ReturnType(_ObjectType::*_Function)(_Params...)>
         void remove(_ObjectType *_object)
         {
-            delegate<_ReturnType(_Params...)> remove_delegate;
-            remove_delegate.bind<_ObjectType, _Function>(_object);
-            m_delegates.remove(remove_delegate);
+            m_delegates.remove(delegate<_ReturnType(_Params...)>::create<_ObjectType, _Function>(_object));
         }
 
         template <class _ObjectType, _ReturnType(_ObjectType::*_Function)(_Params...) const>
         void remove(_ObjectType *_object)
         {
-            delegate<_ReturnType(_Params...)> remove_delegate;
-            remove_delegate.bind<_ObjectType, _Function>(_object);
-            m_delegates.remove(remove_delegate);
+            m_delegates.remove(delegate<_ReturnType(_Params...)>::create<_ObjectType, _Function>(_object));
         }
 
         void invoke(_Params... _params) const
@@ -82,6 +80,11 @@ namespace pw
             for (auto &delegate : m_delegates)
             {
                 delegate.invoke(_params...);
+            }
+
+            for (auto &function : m_functions)
+            {
+                function(_params...);
             }
         }
 
@@ -93,72 +96,7 @@ namespace pw
     private:
 
         std::list<delegate<_ReturnType(_Params...)>> m_delegates;
-    };
-
-    template <typename _ReturnType, typename... _Params>
-    class const_event<_ReturnType(_Params...)>
-    {
-
-    public:
-
-        void add(const_delegate<_ReturnType(_Params...)> &_delegate)
-        {
-            m_delegates.push_back(_delegate);
-        }
-
-        template<_ReturnType(*_Function)(_Params...)>
-        void add()
-        {
-            m_delegates.emplace_back();
-            auto &delegate = m_delegates.back();
-            delegate.bind<_Function>();
-        }
-
-        template <class _ObjectType, _ReturnType(_ObjectType::*_Function)(_Params...) const>
-        void add(_ObjectType *_object)
-        {
-            m_delegates.emplace_back();
-            auto &delegate = m_delegates.back();
-            delegate.bind<_ObjectType, _Function>(_object);
-        }
-
-        void remove(const_delegate<_ReturnType(_Params...)> &_delegate)
-        {
-            m_delegates.remove(_delegate);
-        }
-
-        template<_ReturnType(*_Function)(_Params...)>
-        void remove()
-        {
-            const_delegate<_ReturnType(_Params...)> remove_delegate;
-            remove_delegate.bind<_Function>();
-            m_delegates.remove(remove_delegate);
-        }
-
-        template <class _ObjectType, _ReturnType(_ObjectType::*_Function)(_Params...) const>
-        void remove(_ObjectType *_object)
-        {
-            const_delegate<_ReturnType(_Params...)> remove_delegate;
-            remove_delegate.bind<_ObjectType, _Function>(_object);
-            m_delegates.remove(remove_delegate);
-        }
-
-        void invoke(_Params... _params) const
-        {
-            for (auto &delegate : m_delegates)
-            {
-                delegate.invoke(_params...);
-            }
-        }
-
-        void operator()(_Params... _params) const
-        {
-            this->invoke(_params...);
-        }
-
-    private:
-
-        std::list<const_delegate<_ReturnType(_Params...)>> m_delegates;
+        std::list<std::function<_ReturnType(_Params...)>> m_functions;
     };
 
 } // namespace pw
